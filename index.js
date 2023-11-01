378bf7b70b9cd5859b333bd688ba95e120f5ae65
Joel Pickler <147163113+Jpickler@users.noreply.github.com>
2023-11-01 07:31:27 -0400

    Add files via upload
    converted specific code to a generic function that can take any array and make it an object with element count.

--- index.js
+++ index.js
@@ -12,31 +12,37 @@ let userInput= prompt(`Please enter your order separated by commas no spaces:`, 
 const userOrder=userInput.split(`,`);
 
 // An object is used to keep count of how many orders there are of each flavor.
-const objectOrder={};
+let objectOrder={};
 
+
+const arrayToObject = (arr) =>{
+  const obj=[];
 // A loop is used to iterate through the array of flavors.
 //creates keys that are the flavors
-for(i=0;i<userOrder.length;i++){
-  //console.log(userOrder[i]);
-  let flavor=``;
-    flavor=userOrder[i];
-       //console.log(`this is flavor: ` + flavor);
-       objectOrder[flavor] = 0;
+for(i=0;i<arr.length;i++){
+  //console.log(arr[i]);
+  let ele=``;
+    ele=arr[i];
+       obj[ele] = 0;
   };
 
 
 // The program correctly counts the number of each flavor in the user's input.
 // The logic for counting the frequencies of elements in an array is organized into a function that returns an object.
-  for(i=0;i<userOrder.length;i++){
-    //console.log(userOrder[i]);
-    let flavor=``;
-      flavor=userOrder[i];
-         //console.log(`this is flavor: ` + flavor);
-         objectOrder[flavor]++;
+  for(i=0;i<arr.length;i++){
+    let ele=``;
+      ele=arr[i];
+         obj[ele]++;
     };
+  return(obj);
+};
+
+
+
 
     
 //When they view the browser console, they observe a table listing how many of each flavor they have ordered. 
+objectOrder=arrayToObject(userOrder);
 console.log(`Your Order:`);
 console.log(objectOrder);
 
